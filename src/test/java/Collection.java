76670c651a7a2be37ce9b8338d6ffc01d49442f8
KaretinaA <AleksAnna-92@mail.ru>
2018-10-23 15:06:15 +0300

    Initial commit

---
+++ src/test/java/Collection.java
@@ -0,0 +1,16 @@
+/**
+ * Created by User on 20.10.2018.
+ * * @author Anna Karetina
+ */
+
+
+public class Collection {
+    public static void main(String[] args) {
+        zadanie6 file = new zadanie6();
+        file.openFile();
+        file.readFile();
+        file.outFile();
+        file.getZadanie();
+
+    }
+}
